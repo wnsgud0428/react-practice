587c5e140afb517ca99f6a1e2435750463ea6108
wnsgud0428 <wnsgud0428@naver.com>
2023-05-13 18:52:11 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const incrementCounter = () => setValue((prev) => prev + 1);
-  console.log("call an api"); //state가 바뀌었을때 이 코드가 실행되지 않게 하려면??
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []); //state가 바뀌었을때 이 코드가 실행되지 않게 하려면?? => useEffect 사용!!
 
   return (
     <div>
