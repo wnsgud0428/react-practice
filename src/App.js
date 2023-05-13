e817b80f688f75dfe8151ba82f80968e23aaf6bc
wnsgud0428 <wnsgud0428@naver.com>
2023-05-13 18:46:27 +0900

    state 변화

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Button from "./Button";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const incrementCounter = () => setValue((prev) => prev + 1);
+  console.log("call an api"); //state가 바뀌었을때 이 코드가 실행되지 않게 하려면??
+
   return (
     <div>
-      <h1>Welcome back!</h1>
-      <Button text={1} />
+      <h1>{counter}</h1>
+      <button onClick={incrementCounter}>click me</button>
     </div>
   );
 }
